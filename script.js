6032c17fab9be0af72ec768dcabe932ef3e33c8c
VoxelPrismatic <45671764+VoxelPrismatic@users.noreply.github.com>
2019-09-25 18:17:41 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 function fn() {
   var itm = document.getElementById("coder").innerHTML;
   itm = itm.replace("/<(.|\n)*?>/","");
-  var fns = /(([a-zA-Z][a-zA-Z0-9]*.)+\((.*\n*)*\))/g;
-  var subfn = /(([a-zA-Z][a-zA-Z0-9]*.)+\()(.*\n*)*\)/g
+  var fns = /([\w.]+\([\w\d ]*)\)/mg;
+  var subfn = /([\w.]+)\(([\w\d ]*)\)/;
   var ops = ['%', '+', '*', '/', '-', '^'];
   var bin = ['__builtins__'];
   var kwd = ['import', 'from', 'for', 'while', 'def', 'async'];
   for (var val of ops) {
-    itm.replace(val, '<span class="operator">'+val+'</span>')
+    itm = itm.replace(val, '<span class="operator">'+val+'</span>')
   }
-  for (var fn of itm.match(fns).slice(1,)) {
-    itm = itm.replace(fn, '<span class="function">'+fn.match(subfn)[1]+'</span>'+
-                      fn.match(subfn)[2]+'<span class="function">)</span>");
+  for (var fn of itm.match(fns)) {
+    itm = itm.replace(fn, '<span class="function">'+fn.match(subfn)[1]+'(</span>'+
+                      fn.match(subfn)[2]+'<span class="function">)</span>');
   }
+  for (var fn of bin) {
+    itm = itm.replace(val, '<span class="builtins">'+val+'</span>')
   document.getElementById("coder").innerHTML = itm;
 }
