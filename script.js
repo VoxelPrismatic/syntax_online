1d6c4a87a1641f9e1dd0020c012411327fee7e0f
VoxelPrismatic <45671764+VoxelPrismatic@users.noreply.github.com>
2019-09-25 17:04:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function fn() {
   var itm = document.getElementById("coder").innerHTML;
   itm = itm.replace("/<(.|\n)*?>/","");
-  var fns = /([a-zA-Z0-9]*.)+\((.\n)*\)/;
+  var fns = /(([a-zA-Z0-9]*.)+\((.\n)*\))/;
   var ops = ['%', '+', '*', '/', '-', '^'];
   var bin = ['__builtins__'];
   var kwd = ['import', 'from', 'for', 'while', 'def', 'async'];
