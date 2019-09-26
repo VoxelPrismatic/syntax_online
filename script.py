fca605b10164aa6e6a73e075aff3bb6cca98c68f
VoxelPrismatic <45671764+VoxelPrismatic@users.noreply.github.com>
2019-09-26 09:12:15 -0500

    Update script.py

--- script.py
+++ script.py
@@ -6,9 +6,17 @@ def fn():
     code = sub(r"<(.|\n)*?>", "", code)
     code = sub(r"(\w+)\((.*)\)", r'<span class="function">\1</span>(\2)', code)
     for x in ['%', '**=', '+=', '*=', '%=', '-=', '/=', '**', 
-              '^', '>=', '<=', '+', '/', '\\', '*', '==', '='
-              '>', '<', '&', '|']:
+              '^', '>=', '<=', '+', '//=', '\\', '*', '==',
+              '=',]:
         code = code.replace(x, f'<span class="operand">{x}</span>')
+    for x in ['for ', 'while ', 'import ', 'yield ', 'from ', 'del', 
+              'pass', 'def', 'if ', 'elif ', 'else ', 'try:', 'raise ', 
+              'with ', 'async ', 'finally:', 'except:', 'except ', 
+              'await ', 'class ', ' as ', ' and ', ' or ', ' not ', 
+              ' is ', ' in ', ' False ', ' True ', 'break', 'continue',
+              'lambda ', 'global ', 'assert ', 'nonlocal']:
+        code = code.replace(x, f'<span class="operand">{x}</span>')
+    
     code = sub(r"f'(.*)'", r'<span class="fstring">f\'\1\'</span>', code)
     code = sub(r"r'(.*)'", r'<span class="string">r\'\1\'</span>', code)
     code = sub(r"b'(.*)'", r'<span class="bytes">b\'\1\'</span>', code)
