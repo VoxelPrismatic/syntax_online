3cc6aaf98a965a14033c4d4cc590010d3a98b114
VoxelPrismatic <45671764+VoxelPrismatic@users.noreply.github.com>
2019-09-26 08:53:19 -0500

    Update script.py

--- script.py
+++ script.py
@@ -1,31 +1,19 @@
 from browser import document as doc
 from re import *
 def fn():
-    code = doc.getElementById("coder").innerHTML
+    codeA = doc.getElementById("coder").innerHTML
+    code = codeA
     code = sub(r"<(.|\n)*?>", "", code)
     code = sub(r"( \w+)\(.*\)", r'<span class="function">\1</span>', code)
     for x in ['%', '**=', '+=', '*=', '%=', '-=', '/=', '**', 
               '^', '>=', '<=', '+', '/', '\\', '*', '==', '='
-              '>', '<', '&']:
+              '>', '<', '&', '|']:
         code = code.replace(x, f'<span class="operand">{x}</span>')
     code = sub(r"f'(.*)'", r'<span class="fstring">f\'\1\'</span>', code)
-           
-function fn() {
-  var itm = document.getElementById("coder").innerHTML;
-  itm = itm.replace("/<(.|\n)*?>/","");
-  var fns = /([\w.]+\([\w\d ]*)\)/mg;
-  var subfn = /([\w.]+)\(([\w\d ]*)\)/;
-  var ops = ['%', '+', '*', '/', '-', '^'];
-  var bin = ['__builtins__'];
-  var kwd = ['import', 'from', 'for', 'while', 'def', 'async'];
-  for (var val of ops) {
-    itm = itm.replace(val, '<span class="operator">'+val+'</span>')
-  }
-  for (var fn of itm.match(fns)) {
-    itm = itm.replace(fn, '<span class="function">'+fn.match(subfn)[1]+'(</span>'+
-                      fn.match(subfn)[2]+'<span class="function">)</span>');
-  }
-  for (var fn of bin) {
-    itm = itm.replace(val, '<span class="builtins">'+val+'</span>')
-  document.getElementById("coder").innerHTML = itm;
-}
+    code = sub(r"b'(.*)'", r'<span class="bytes">b\'\1\'</span>', code)
+    code = sub(r"f'(.*)'", r'<span class="string">\'\1\'</span>', code)
+    code = sub(r'f"(.*)"', r'<span class="fstring">f"\1"</span>', code)
+    code = sub(r'b"(.*)"', r'<span class="bytes">b"\1"</span>', code)
+    code = sub(r'"(.*)"', r'<span class="string">"\1"</span>', code)
+    if codeA != code:
+        doc.getElementById("coder").innerHTML = code
