a6ac91deedf3c735f7a5590778285ee62d79faa3
VoxelPrismatic <45671764+VoxelPrismatic@users.noreply.github.com>
2019-09-27 17:24:00 -0500

    Update py.js

--- py.js
+++ py.js
@@ -1,34 +1,40 @@
 
 doc = document;
 function sp(inp, typ) {
-    return '\<span class="'+typ+'"\>'+inp+'\<\/span\>';
+    return `<span class="${typ}">`+inp+'</span>';
 }
-function regex2(reg, st, rep) {
-    match =  st.match(reg);
-    if (match != null) {
-        rep = rep.replace("${match[1]}", match[1]);
-        rep = rep.replace("${match[2]}", match[2]);
-        st = st.replace(reg, rep);
-    return st;
+function rm(st) {
+    return regex(/<span class=".*">(.*)<\/span>/, st, "${abcdWXYZ[1]}");
 }
-function regex1(reg, st, rep) {
-    match =  st.match(reg);
+function regex(reg, st, rep) {
+    match = st.match(reg);
     if (match != null) {
-        rep = rep.replace("${match[1]}", match[1]);
+        for (var x = 1; x < match.length; x++)
+            rep = rep.replace("${abcdWXYZ["+x+"]}", match[x]);
         st = st.replace(reg, rep);
+    }
     return st;
 }
 function fn(){
     codeA = doc.getElementById("coder").innerHTML;
-    code = codeA;
-    code = code.replace(/<(.|\n)*?>/, "");
-    
-    code = regex2(/(\w+)\((.*)\)/, sp("${match[1]}", "function")+"(${match[2]})");
-    var sym = ['%', '^', '+', '*', '=', '>', '<', '~', '|', '@', '[',
-               ']', '{', '}', '(', ')', '&', ',', '.', ';', ':', '__',
-               '!', '?', '`', '-', 'j', 'J', '&'];
-    for (var x of sym)
-        code = code.replace(x, sp(x, "operand"));
+    code = codeA+" ";
+    code = rm(code);
+    code = regex(/(\w+)\((.*)\)/m, code, sp("${abcdWXYZ[1]}", "function")+"(${abcdWXYZ[2]})");
+    code = regex(/"(.*)"[^>]/, code, sp('"${abcdWXYZ[1]}"', "string"));
+    code = regex(/'(.*)'[^>]/, code, sp("'${abcdWXYZ[1]}'", "string"));
+    code = regex(/([fF])<span class="string">"(.*)"<\/span>/, code, 
+                  sp('${abcdWXYZ[1]}"${abcdWXYZ[2]}"', "fstring"));
+    code = regex(/([bBrR])<span class="string">"(.*)"<\/span>/, code, 
+                  sp('${abcdWXYZ[1]}"${abcdWXYZ[2]}"', "bytes"));
+    code = regex(/([uU])<span class="string">"(.*)"<\/span>/, code, 
+                  sp('${abcdWXYZ[1]}"${abcdWXYZ[2]}"', "string"));
+    code = regex(/([fF])<span class="string">'(.*)'<\/span>/, code, 
+                  sp("${abcdWXYZ[1]}'${abcdWXYZ[2]}'", "fstring"));
+    code = regex(/([bBrR])<span class="string">"(.*)"<\/span>/, code, 
+                  sp("${abcdWXYZ[1]}'${abcdWXYZ[2]}'", "bytes"));
+    code = regex(/([uU])<span class="string">"(.*)"<\/span>/, code, 
+                  sp("${abcdWXYZ[1]}'${abcdWXYZ[2]}'", "string"));
+    code = regex(/__(.*)__/, code, sp("<i>__${abcdWXYZ[1]}__</i>", "operand"));
     var kws= ['for', 'while', 'import', 'yield', 'from', 'del', 
               'pass', 'def', 'if', 'elif', 'else', 'try', 'raise', 
               'with', 'async', 'finally', 'except', 'except', 
@@ -36,16 +42,12 @@ function fn(){
               'is', 'in', 'False', 'True', 'break', 'continue',
               'lambda ', 'global', 'assert', 'nonlocal'];
     for (var x of kws)
-        code = code.replace(x, sp(x, "builtin"));
-    code = regex2(/([fF])'(.*)'/, sp("${match[1]}'${match[2]}'", "fstring"));
-    code = regex2(/([bBrR])'(.*)'/, sp("${match[1]}'${match[2]}'", "bytes"));
-    code = regex2(/([uU])'(.*)'/, sp("${match[1]}'${match[2]}'", "string"));
-    code = regex1(/'(.*)'/, sp("'${match[1]}'", "string"));
-    code = regex2(/([fF])"(.*)"/, sp('${match[1]}"${match[2]}"', "fstring"));
-    code = regex2(/([bBrR])"(.*)"/, sp('${match[1]}"${match[2]}"', "bytes"));
-    code = regex2(/([uU])"(.*)"/, sp('${match[1]}"${match[2]}"', "string"));
-    code = regex1(/"(.*)"/, sp('"${match[1]}"', "string"));
-    if (codeA != code)
-        doc.getElementById("coder").innerHTML = code;
+        code = code//.replace(x, sp(`<b>${x}</b>`, "builtin"));
+    code = regex(/#(.*)/, code, sp("#${abcdWXYZ[1]}", "comment"));
+    match = code.match(/(<.*>).*<.*>.*(.*).*<\/.*>.*(<\/.*>)/);
+    code = regex(/<span class="(.*)">(.*)<span class=".*">(.*)<\/span>(.*)<\/span>/, code, 
+                  sp("${abcdWXYZ[2]}${abcdWXYZ[3]}${abcdWXYZ[4]}", "${abcdWXYZ[1]}"));
+    //code = regex1(/<span class="comment"><.*>(.*)<.*>
+    doc.getElementById("coder").innerHTML = code;
 }
 doc.getElementById("coder").addEventListener("blur", fn, true)
