e489c1a5d5f1ad20c6cdc9a5cbfb44c669f16868
VoxelPrismatic <45671764+VoxelPrismatic@users.noreply.github.com>
2019-10-10 15:22:38 -0500

    Update fns.py

--- fns.py
+++ fns.py
@@ -1,16 +1,23 @@
 from browser import document as doc, window as win, html
-def gID(elem):
+def gID(elem): 
     return doc[elem]
-def gHTML(elem):
+
+def gHTML(elem): 
     return gID(elem)
-def gSTYLE(elem, sty):
+
+def gSTYLE(elem, sty): 
     return gID(elem).style[sty]
 def eEDIT(elem, val):
     gID(elem).text = val
-def eST_EDIT(elem, sty, val):
+    
+def eSTYLE(elem, sty, val):
     gID(elem).style[sty] = val
+    
 def eCLASS(elem, nam):
     if nam in gID(elem).classList:
         gID(elem).classList.remove(nam)
     else:
         gID(elem).classList.add(nam)
+        
+def gCLASS(nam):
+    return doc.select(f".{nam}")
