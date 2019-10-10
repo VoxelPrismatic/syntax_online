57ca33cee6ab72c52f900786ecaf1a9fb728f94f
VoxelPrismatic <45671764+VoxelPrismatic@users.noreply.github.com>
2019-10-10 15:17:11 -0500

    Update fns.py

--- fns.py
+++ fns.py
@@ -4,4 +4,13 @@ def gID(elem):
 def gHTML(elem):
     return gID(elem)
 def gSTYLE(elem, sty):
-    return 
+    return gID(elem).style[sty]
+def eEDIT(elem, val):
+    gID(elem).text = val
+def eST_EDIT(elem, sty, val):
+    gID(elem).style[sty] = val
+def eCLASS(elem, nam):
+    if nam in gID(elem).classList:
+        gID(elem).classList.remove(nam)
+    else:
+        gID(elem).classList.add(nam)
