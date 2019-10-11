af4bd0a5491e49421355cebc3d71691211e85bbf
VoxelPrismatic <45671764+VoxelPrismatic@users.noreply.github.com>
2019-10-11 13:46:19 -0500

    Update py.py

--- py.py
+++ py.py
@@ -1,6 +1,11 @@
 from browser import window as win, document as doc, html
-from re import sub, match
+from re import sub as sb, match
 from fns import *
+
+def sub(a, b, c):
+    c = sb(a, b, c)
+    doc["coder"].innerHTML = c
+    return c
 
 def rm():
     st = gID("coder").innerHTML
@@ -11,13 +16,7 @@ def rm():
 def fn(*args):
     rm()
     code = doc["coder"].innerHTML.replace("<div>", '\n').replace("</div>", '\t');
-    code = sub(r"'(.*)'", r"<s>'\1'<<>", code)
-    code = sub(r"([uUfF])'(.*)'", r"<d>\1'\2'<<>", code)
-    code = sub(r"([rRuU])'(.*)'", r"<x>\1'\2'<<>", code)
-    code = sub(r'"(.*)"', r'<s>"\1"<<>', code)
-    code = sub(r'([uUfF])"(.*)"', r'<d>\1"\2"<<>', code)
-    code = sub(r'([rRuU])"(.*)"', r'<x>\1"\2"<<>', code)
-    code = sub(r'((\w+.)*)\(', r'<f>\1<<>\(', code)
+    code = sub(r'((\w+\.)*)\(', r'<f>\1<<>(', code)
     code = sub(r'__(.*)__', r'<i>__\1__</i>', code)
     for kw in ['for', 'while', 'import', 'yield', 'from', 'del', 
                'pass', 'def', 'if', 'elif', 'else', 'try', 'raise',
@@ -26,13 +25,18 @@ def fn(*args):
                'is', 'in', 'False', 'True', 'break', 'continue',
                'lambda ', 'global', 'assert', 'nonlocal']:
         code = code.replace(kw, f"<b><o>{kw}<<><\b>")
+    code = sub(r"([uUfF])'(.*)'", r"<d>\1'\2'<<>", code)
+    code = sub(r"([rRuU])'(.*)'", r"<x>\1'\2'<<>", code)
+    code = sub(r'([uUfF])"(.*)"', r'<d>\1"\2"<<>', code)
+    code = sub(r'([rRuU])"(.*)"', r'<x>\1"\2"<<>', code)
+    code = sub(r"'(.*)'", r"<s>'\1'<<>", code)
+    code = sub(r'"(.*)"', r'<s>"\1"<<>', code)
     code = sub(r"\#(.*)", r"<c>\#\1<<>", code)
     code = code.replace("<f>", '<span class="function">').replace("<s>", '<span class="string">')
     code = code.replace("<t>", '<span class="builtin">').replace("<o>", '<span class="operand">')
     code = code.replace("<c>", '<span class="comment">').replace("<x>", '<span class="bytes">')
     code = code.replace("<d>", '<span class="fstring">').replace("<<>", '</span>');
-    #code = regex(/<span class="(.*)">(.*)<span class=".*">(.*)<\/span>(.*)<\/span>/, code, sp("${2}${3}${4}", "${1}"));
+    code = sub(r'<span class="(.*)">(.*)<span class=".*">(.*)<\/span>(.*)<\/span>', 
+               r'<span class="\1">\2\3\4</span', code);
     doc["coder"].innerHTML = code.replace("\n","<div>").replace("\t","</div>");
-    #console.log(doc.getElementById("coder").innerHTML);
-
 doc["coder"].bind("focusout", fn);
